18f47a3ab5a018f8bb2817c340fec88997eb96c6
Adriel Santana <diel_san@hotmail.com>
2022-08-28 00:51:33 -0300

    Inicio do front end client, header feito

--- javascript_note/src/App.js
+++ javascript_note/src/App.js
@@ -1,27 +1,15 @@
 import React, { Fragment } from 'react'
 import './App.scss';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+import Header from './components/header';
+
+import Container from 'react-bootstrap/esm/Container';
 
 const App = () => {
   return (
     <Fragment>
-      <Container>
-        <Row className='text-center'>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-          <Col className='test' lg={{ span: 1, offset: 0 }}>lg</Col>
-        </Row>
+      <Container className='purple-gradient-bg main-container' fluid>
+      <Header/>
       </Container>
     </Fragment>
   )
---
+++ javascript_note/src/components/header/index.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useState } from 'react';
+import Container from 'react-bootstrap/esm/Container';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button'
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import '../../assets/styles/header.scss'
+
+import hamburguer from '../../assets/images/hamburguer.svg'
+import logoImage from '../../assets/images/logo.svg'
+
+function Header() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    return (
+        <Fragment>
+            <Container>
+                <Nav className="justify-content-between align-items-center">
+
+                    <span className='d-flex'>
+                        <Nav.Item className='align-items-center'>
+                            <a href='/#'>
+                                <img src={logoImage} className='brand' alt='logo'>
+                                </img>
+                            </a>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <a href='/#' className='font-3 courgette brand-title'>
+                                Tomenote
+                            </a>
+                        </Nav.Item>
+                    </span>
+
+                    <Offcanvas placement='end' show={show} onHide={handleClose} responsive="md">
+                        <Offcanvas.Header closeButton closeVariant='white'>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <span className='d-flex justify-content-center align-items-center'>
+                                <Nav.Item>
+                                    <a href='/#' className='cta brand-title'>
+                                        <span class="nunito hover-underline-animation">
+                                            Entrar
+                                        </span>
+                                    </a>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Button className='orange-gradient-bg register-button nunito'>
+                                        Registrar
+                                    </Button>
+                                </Nav.Item>
+                            </span>
+                        </Offcanvas.Body>
+                    </Offcanvas>
+
+                    <Button variant="link" className="d-md-none hamburguer-button" onClick={handleShow}>
+                        <img alt='hamburguer' className='hamburguer' src={hamburguer}></img>
+                    </Button>
+                </Nav>
+            </Container>
+        </Fragment>
+    )
+}
+
+export default Header
