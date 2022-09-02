7b19a9cfb14257b91e198bbf74f92b1357d6af1d
Adriel Santana <diel_san@hotmail.com>
2022-09-02 13:45:00 -0300

    iniciando deploy

--- tomenote_api/config/database.js
+++ tomenote_api/config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/javascriptNote').then(() => {
+require('dotenv').config()
+const MONGO_URL = process.env.MONGO_URL
+
+mongoose.connect(MONGO_URL).then(() => {
     console.log('Conection Succesfull')
 }).catch((error) => {
     console.log(error)
