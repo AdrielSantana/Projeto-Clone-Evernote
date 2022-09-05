60608579b56ee95292a99f9390fdb1228a526388
Adriel Santana <diel_san@hotmail.com>
2022-09-05 20:13:13 -0300

    adição de scroll to top button

--- tomenote/src/screens/notes/index/index.js
+++ tomenote/src/screens/notes/index/index.js
@@ -2,18 +2,16 @@ import React, { Fragment } from 'react'
 
 import Header from '../../../components/header_logged';
 import Notes from '../../../components/notes';
+import ScrollToTop from '../../../components/scroll_to_top_button';
 
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 
 const NotesScreen = () => {
-
-
     return (
         <Fragment>
-
+            <ScrollToTop />
             <Row className='main-container d-flex' xs={1}>
-
                 <Col className='d-flex flex-column align-self-start'>
 
                     <div className='sticky-top purple-gradient-bg header-notes'>
