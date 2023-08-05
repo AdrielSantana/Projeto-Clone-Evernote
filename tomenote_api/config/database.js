7986eb7d4e2a97a355d1104ad2c5af54740ff457
Adriel Santana <diel_san@hotmail.com>
2023-08-05 16:33:42 -0300

    testar

--- tomenote_api/config/database.js
+++ tomenote_api/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 require('dotenv').config()
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGODB_URI
 
 mongoose.connect(MONGO_URL).then(() => {
     console.log('Conection Succesfull')
